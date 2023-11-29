c1fc0fc37ca4cfdc168eda194d5951884d87d68d
vietchinh <1348151+vietchinh@users.noreply.github.com>
2023-11-29 18:56:14 +0100

    fix: initialize distrobox fedora kde at boot once

--- config/scripts/ucore-kde.sh
+++ config/scripts/ucore-kde.sh
@@ -6,6 +6,14 @@
 set -oue pipefail
 
 # Your code goes here.
+mkdir -p /usr/etc/profile.d/
+echo "chown -f -R $USER:$USER /tmp/.X11-unix" >> /usr/etc/profile.d/fix_tmp.sh
+
+mkdir -p /usr/etc/distrobox-fedora-kde/
+
+cat > /usr/etc/distrobox-fedora-kde/initialize_distrobox_fedora_kde.sh << EOF
+#!/usr/bin/bash
+
 CONTAINER_NAME="fedora-kde"
 
 distrobox create --yes --name $CONTAINER_NAME --image registry.fedoraproject.org/fedora:latest
@@ -27,18 +35,26 @@ flatpak remote-add --user --if-not-exists flathub https://dl.flathub.org/repo/fl
 exit
 
 distrobox-export --bin /var/lib/flatpak/ --export-path /var/lib/flatpak/
-
-mkdir -p /usr/etc/profile.d/
-echo "chown -f -R $USER:$USER /tmp/.X11-unix" >> /usr/etc/profile.d/fix_tmp.sh
-
-cat > ~/.profile << EOF
-if [ -f /usr/libexec/kactivitymanagerd ]; then
-  /usr/libexec/kactivitymanagerd & disown
-fi
+systemctl --user enable --now fedora-kde-plasma-autostart.service
+systemctl --user disable initialize-distrobox-fedora-kde.service
 EOF
 
+chmod +x /usr/etc/distrobox-fedora-kde/initialize_distrobox_fedora_kde.sh
 
-cat > /usr/lib/systemd/system/fedora-kde-plasma-autostart.service << EOF
+cat > /usr/lib/systemd/user/initialize-distrobox-fedora-kde.service << EOF
+[Unit]
+Description=Autostart kde plasma
+ConditionPathExists=/run/ostree-booted
+ConditionFileIsExecutable=/etc/distrobox-fedora-kde/initialize_distrobox_fedora_kde.sh
+
+[Service]
+Type=oneshot
+StandardOutput=journal+console
+RemainAfterExit=no
+ExecStart=/etc/distrobox-fedora-kde/initialize_distrobox_fedora_kde.sh
+EOF
+
+cat > /usr/lib/systemd/user/fedora-kde-plasma-autostart.service << EOF
 [Unit]
 Description=Autostart kde plasma
 ConditionPathExists=/run/ostree-booted
