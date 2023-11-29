5a387cbee5098ec45429839d732e97a5b4d3c77d
vietchinh <1348151+vietchinh@users.noreply.github.com>
2023-11-29 18:35:59 +0100

    fix: install flatpak inside container

--- config/scripts/ucore-kde.sh
+++ config/scripts/ucore-kde.sh
@@ -11,17 +11,22 @@ CONTAINER_NAME="fedora-kde"
 distrobox create --name $CONTAINER_NAME --image registry.fedoraproject.org/fedora:latest
 distrobox enter $CONTAINER_NAME
 
+sudo umount /var/lib/flatpak
 sudo umount /run/systemd/system
 sudo rmdir /run/systemd/system
 sudo ln -s /run/host/run/systemd/system /run/systemd
 sudo ln -s /run/host/run/dbus/system_bus_socket /run/dbus/
 ln -s /usr/bin/distrobox-host-exec /usr/local/bin/podman
 ln -s /usr/bin/distrobox-host-exec /usr/local/bin/flatpak
-flatpak remote-add --user --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrep0
 
 sudo dnf groupinstall KDE -y
+sudo dnf install flatpak -y
+
+flatpak remote-add --user --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
 exit
+
+distrobox-export --bin /var/lib/flatpak/ --export-path /var/lib/flatpak/
 
 mkdir -p /usr/etc/profile.d/
 echo "chown -f -R $USER:$USER /tmp/.X11-unix" >> /usr/etc/profile.d/fix_tmp.sh
