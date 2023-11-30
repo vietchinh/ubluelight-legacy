b7082d0295111809e6b99ad328f701df7da05799
vietchinh <1348151+vietchinh@users.noreply.github.com>
2023-11-30 19:55:37 +0100

    feat: attempt to build container beforehand

--- config/scripts/ucore-kde.sh
+++ config/scripts/ucore-kde.sh
@@ -6,8 +6,24 @@
 set -oue pipefail
 
 # Your code goes here.
+DBX_CONTAINER_HOME_PREFIX=/usr/etc/distrobox-fedora-kde
+
 rpm -ivh /tmp/ublue-os-signing.noarch.rpm
-chmod +x /usr/etc/distrobox-fedora-kde/initialize_distrobox_fedora_kde.sh
+chmod +x "$DBX_CONTAINER_HOME_PREFIX/initialize_distrobox_fedora_kde.sh"
+
+distrobox assemble create --file /usr/etc/distrobox-fedora-kde/fedora-kde.ini
+
+distrobox enter --root fedora-kde
+
+sudo ln -s /run/host/run/systemd/system /run/systemd
+sudo mkdir /run/dbus
+sudo ln -s /run/host/run/dbus/system_bus_socket /run/dbus/
+
+sudo dnf install @KDE flatpak -your
+
+exit
+
+
 
 cat > /usr/lib/systemd/system/initialize-distrobox-fedora-kde.service << EOF
 [Unit]
