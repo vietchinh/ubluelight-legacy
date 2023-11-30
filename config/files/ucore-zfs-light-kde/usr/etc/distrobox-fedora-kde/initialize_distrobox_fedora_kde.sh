7870c94b6ea0c7730686252c2d89e85f9b52fd73
vietchinh <1348151+vietchinh@users.noreply.github.com>
2023-11-30 20:27:02 +0100

    fix: attempt to fix container build error 3

--- config/files/ucore-zfs-light-kde/usr/etc/distrobox-fedora-kde/initialize_distrobox_fedora_kde.sh
+++ config/files/ucore-zfs-light-kde/usr/etc/distrobox-fedora-kde/initialize_distrobox_fedora_kde.sh
@@ -2,6 +2,16 @@
 
 distrobox assemble create --file /usr/etc/distrobox-fedora-kde/fedora-kde.ini
 
+distrobox enter --root fedora-kde
+
+sudo ln -s /run/host/run/systemd/system /run/systemd
+sudo mkdir /run/dbus
+sudo ln -s /run/host/run/dbus/system_bus_socket /run/dbus/
+
+sudo dnf install @KDE flatpak -your
+
+exit
+
 sudo mkdir -p /etc/profile.d/
 echo "chown -f -R $USER:$USER /tmp/.X11-unix" | sudo tee -a /etc/profile.d/fix_tmp.sh
 
--- config/scripts/ucore-kde.sh
+++ config/scripts/ucore-kde.sh
@@ -7,24 +7,9 @@ set -oue pipefail
 
 # Your code goes here.
 DBX_CONTAINER_HOME_PREFIX=/usr/etc/distrobox-fedora-kde
-DBX_CONTAINER_CUSTOM_HOME=/usr/etc/distrobox-fedora-kde
 
 #rpm -ivh /tmp/ublue-os-signing.noarch.rpm
 chmod +x "$DBX_CONTAINER_HOME_PREFIX/initialize_distrobox_fedora_kde.sh"
-
-distrobox assemble create --file /usr/etc/distrobox-fedora-kde/fedora-kde.ini
-
-distrobox enter --root fedora-kde
-
-sudo ln -s /run/host/run/systemd/system /run/systemd
-sudo mkdir /run/dbus
-sudo ln -s /run/host/run/dbus/system_bus_socket /run/dbus/
-
-sudo dnf install @KDE flatpak -your
-
-exit
-
-
 
 cat > /usr/lib/systemd/system/initialize-distrobox-fedora-kde.service << EOF
 [Unit]
