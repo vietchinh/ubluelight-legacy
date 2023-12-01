bcc2d38e20022ee2114c7593b24f9b5752e2443e
vietchinh <1348151+vietchinh@users.noreply.github.com>
2023-12-01 04:13:09 +0100

    fix: testing

--- config/files/ucore-zfs-light-kde/usr/etc/profile.d/initialize_distrobox_fedora_kde.sh
+++ config/files/ucore-zfs-light-kde/usr/etc/profile.d/initialize_distrobox_fedora_kde.sh
@@ -2,23 +2,21 @@
 
 distrobox assemble create --file /usr/etc/distrobox-fedora-kde/fedora-kde.ini
 
-distrobox enter fedora-kde
-
-sudo ln -s /run/host/run/systemd/system /run/systemd
-sudo mkdir /run/dbus
-sudo ln -s /run/host/run/dbus/system_bus_socket /run/dbus/
-
-sudo dnf install @KDE flatpak -y
-
-exit
+#distrobox enter fedora-kde --
+#
+#sudo ln -s /run/host/run/systemd/system /run/systemd
+#sudo mkdir /run/dbus
+#sudo ln -s /run/host/run/dbus/system_bus_socket /run/dbus/
+#
+#sudo dnf install @KDE flatpak -y
 
 rpm-ostree install sddm sddm-breeze sddm-kcm
 
-sudo mkdir -p /etc/profile.d/
-echo "chown -f -R $USER:$USER /tmp/.X11-unix" | sudo tee -a /etc/profile.d/fix_tmp.sh
+#sudo mkdir -p /etc/profile.d/
+#echo "chown -f -R $USER:$USER /tmp/.X11-unix" | sudo tee -a /etc/profile.d/fix_tmp.sh
 
 distrobox-export --bin /var/lib/flatpak/ --export-path /var/lib/flatpak/
 
 sudo systemctl enable --now sddm.service
 
-rm -- "$0"
+rm -f "/usr/etc/profile.d/initialize_distrobox_fedora_kde.sh"
